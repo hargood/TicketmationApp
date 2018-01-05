9b961bce4efac9491a1dae3b5cd54ebb626e4381
Harvey Goodman <hgoodman@cgc-intl.com>
2018-01-05 15:37:53 -0800

    DB Path

--- TicketmationApp/MiTrade.vb
+++ TicketmationApp/MiTrade.vb
@@ -5,7 +5,7 @@
     Public strComputerName As String
     Public intComPort As Integer
     Public SessionCounter As String
-    Public strConnectionString As String = "MSAccessOP"
+    Public strConnectionString As String = "MSAccess"
     Public bHaveInternet As Boolean = True
     'DATABASE
     '    Public con As New OledbConnection("Provider=microsoft.Jet.oledb.4.0DataSource=D:\mydata.mdb;")
